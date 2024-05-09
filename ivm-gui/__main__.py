7d84a198d50a8739593b58bec30204d336c6fbda
Davide Ciminaghi <ciminaghi@gnudd.com>
2024-05-09 10:07:17 +0200

    first trivial qt5 application

---
+++ ivm-gui/__main__.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+# Only needed for access to command line arguments
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Test App")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = MainWindow()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec()
